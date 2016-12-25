27ed802edb9ef167cbdac247a83ae86ebfaaea3e
tanlaan <tanlaan33@gmail.com>
2016-12-25 08:01:43 +0000

    hit a road block, saving before complete change

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,18 @@
 #write your code here
+def translate(word)
+    characters = word.split('')
+    first_letter = characters[0]
+    case first_letter
+    when 'a','e','i','o','u'
+        characters.push("ay")
+    else
+        a = characters.slice!(0)
+        characters.push(a)
+        characters.push("ay")
+        
+        
+    end
+    word = characters.join('')
+    word
+    
+end 
