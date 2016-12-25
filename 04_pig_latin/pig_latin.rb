f38a6457ec62f12563488fa9620349424f939db8
tanlaan <tanlaan33@gmail.com>
2016-12-25 08:15:26 +0000

    figured out a few parts, have to arrange for a third check for consonants

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,18 +1,29 @@
 #write your code here
-def translate(word)
-    characters = word.split('')
-    first_letter = characters[0]
-    case first_letter
-    when 'a','e','i','o','u'
-        characters.push("ay")
-    else
-        a = characters.slice!(0)
-        characters.push(a)
-        characters.push("ay")
-        
-        
-    end
-    word = characters.join('')
-    word
+def translate(sentence)
+    vowels = ['a','e','i','o','u']
+    piglatin = []
     
+    words = sentence.split(' ')
+    words.each do |word|
+        characters = word.split('')
+        first_letter = characters[0]
+        case first_letter
+        when 'a','e','i','o','u'
+        else
+            if vowels.include? characters[1]
+                a = characters.slice!(0)
+                characters.push(a)
+            elsif 
+            else
+                a = characters.slice!(0..1)
+                characters.push(a)
+            end
+        end
+        characters.push("ay")
+        characters = characters.join('')
+        piglatin.push(characters)
+    end
+
+    piglatin = piglatin.join(' ')
+    piglatin
 end 
