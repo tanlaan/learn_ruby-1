f1498155001decb22831b8f7754d72dcce4886be
tanlaan <tanlaan33@gmail.com>
2016-12-26 05:55:46 +0000

    Figured out the if/else tree for pig latin project

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -6,22 +6,40 @@ def translate(sentence)
     words = sentence.split(' ')
     words.each do |word|
         characters = word.split('')
-        first_letter = characters[0]
-        case first_letter
-        when 'a','e','i','o','u'
-        else
+        if vowels.include? characters[0]
+            #do nothing, starts with vowel
+        else 
             if vowels.include? characters[1]
-                a = characters.slice!(0)
-                characters.push(a)
-            elsif 
+                if characters.slice(0..1).join('') == 'qu'
+                    #exception for qu, take the first two letters to the end
+                    a = characters.slice!(0..1)
+                    characters.push(a)
+                else
+                   #take the first letter to the end
+                    a = characters.slice!(0)
+                    characters.push(a)
+                end
             else
-                a = characters.slice!(0..1)
-                characters.push(a)
-            end
+                if vowels.include? characters[2]
+                    if characters.slice(1..2).join('') == 'qu'
+                        #exception for qu as a "single" consenant 
+                        a = characters.slice!(0..2)
+                        characters.push(a)
+                    else
+                        #take the first two letters to the end
+                        a = characters.slice!(0..1)
+                        characters.push(a)
+                    end
+                else
+                   #take the first three letters to the end
+                   a = characters.slice!(0..2)
+                   characters.push(a)
+                end
+            end 
         end
         characters.push("ay")
-        characters = characters.join('')
-        piglatin.push(characters)
+        word = characters.join('')
+        piglatin.push(word)
     end
 
     piglatin = piglatin.join(' ')
